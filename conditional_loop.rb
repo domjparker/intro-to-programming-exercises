0c54f112f1a1b52440cfb7827fe4288e7daad055
domjparker <dominic.j.parker@gmail.com>
2019-04-06 11:44:17 -0700

    file from Loops & Iterators chapter

--- conditional_loop.rb
+++ conditional_loop.rb
@@ -9,21 +9,4 @@ loop do
   end
 end
 
-puts ""
-puts "______________________________________"
-puts ""
-
-# next_loop
-
-i = 0
-loop do 
-  i += 2
-  if i == 4
-    next
-  end
-  puts i 
-  if i == 10
-    break
-  end
-end
  
